eecc8946e1ebfce37df3ec36fd6b4c0a68bcd8c6
Tobias Assarsson <tobias.assarsson@gmail.com>
2025-07-03 11:23:18 +0200

    Use event enum instead of constants for event switching and put fps printout loop behind flag or signal

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use socket2::{Domain, Socket, Type};
 use std::io::Read;
 use std::net::SocketAddr;
 use std::time::Duration;
+use num_derive::FromPrimitive;
 
 mod dmx;
 mod serial;
@@ -33,6 +34,9 @@ struct Args {
     // Throttle DMX writes to avoid flooding
     #[arg(long, short, default_value = "45")]
     throttle: u64,
+
+    #[arg(long, short)]
+    debug: bool,
 }
 
 fn get_domain(socket_addr: SocketAddr) -> Domain {
@@ -42,10 +46,14 @@ fn get_domain(socket_addr: SocketAddr) -> Domain {
     }
 }
 
-const EVENT_SIGNAL_NUM: u64 = 0; // SignalFd event number
-const EVENT_UDP_SOCKET_NUM: u64 = 1; // UDP socket event number
-const EVENT_DMX_PORT_NUM: u64 = 2; // DMX port event number
-const EVENT_TICK_NUM: u64 = 3; // TimerFd event number
+#[derive(FromPrimitive)]
+#[repr(u64)]
+enum Event {
+    Signal = 1,
+    UDP = 2,
+    DMX = 3,
+    Tick = 4,
+}
 
 fn main() -> std::io::Result<()> {
     let mut exiting = false; // Flag to indicate if the program is exiting
@@ -79,19 +87,19 @@ fn main() -> std::io::Result<()> {
     let epoll = Epoll::new(EpollCreateFlags::empty())?;
     epoll.add(
         &sigfd,
-        EpollEvent::new(EpollFlags::EPOLLIN, EVENT_SIGNAL_NUM),
+        EpollEvent::new(EpollFlags::EPOLLIN, Event::Signal as u64),
     )?;
     epoll.add(
         &socket,
-        EpollEvent::new(EpollFlags::EPOLLIN, EVENT_UDP_SOCKET_NUM),
+        EpollEvent::new(EpollFlags::EPOLLIN, Event::UDP as u64),
     )?;
     epoll.add(
         &dmx_port,
-        EpollEvent::new(EpollFlags::EPOLLOUT, EVENT_DMX_PORT_NUM),
+        EpollEvent::new(EpollFlags::EPOLLOUT, Event::DMX as u64),
     )?;
     epoll.add(
         &ticker,
-        EpollEvent::new(EpollFlags::EPOLLIN, EVENT_TICK_NUM),
+        EpollEvent::new(EpollFlags::EPOLLIN, Event::Tick as u64),
     )?;
 
     let mut event_buffer = [EpollEvent::empty(); 10];
@@ -100,6 +108,8 @@ fn main() -> std::io::Result<()> {
     let mut udp_frames = 0; // Counter for UDP frames received
     let mut last_dmx_frames = 0; // Last count of DMX frames processed
     let mut last_udp_frames = 0; // Last count of UDP frames received
+    let mut last_udp_delta = 0;
+    let mut last_dmx_delta = 0;
 
     let mut dmx_write_throttle = TimeSpec::zero();
 
@@ -109,8 +119,15 @@ fn main() -> std::io::Result<()> {
             if exiting {
                 break;
             }
-            match event.data() {
-                EVENT_SIGNAL_NUM => {
+            let event = match num::FromPrimitive::from_u64(event.data()){
+                Some(val) => val,
+                None => {
+                    println!("Received unknown event number: {:?}", event.data());
+                    continue;
+                }
+            };
+            match event {
+                Event::Signal => {
                     if let Some(info) = sigfd.read_signal()? {
                         match (info.ssi_signo as i32).try_into() {
                             Ok(Signal::SIGINT) => {
@@ -123,6 +140,9 @@ fn main() -> std::io::Result<()> {
                                 println!("Received SIGTERM signal, exiting...");
                                 exiting = true;
                             }
+                            Ok(Signal::SIGUSR1) => {
+                                println!("FPS[UDP:{:?} DMX:{:?}]", last_udp_delta, last_dmx_delta);
+                            }
                             _ => {
                                 // Handle other signals
                                 println!("Received signal: {:?}", info.ssi_signo);
@@ -130,14 +150,14 @@ fn main() -> std::io::Result<()> {
                         }
                     }
                 }
-                EVENT_UDP_SOCKET_NUM => {
+                Event::UDP => {
                     // This is a non-blocking socket
                     // We need to drain all buffered frames to catch up
                     while let Ok(_size) = socket.read(&mut dmx_data[1..]) {
                         udp_frames += 1; // Increment UDP frame count
                     }
                 }
-                EVENT_DMX_PORT_NUM => {
+                Event::DMX => {
                     let now = nix::time::clock_gettime(nix::time::ClockId::CLOCK_MONOTONIC)?;
                     if now - dmx_write_throttle
                         < TimeSpec::from(Duration::from_millis(1000 / args.throttle))
@@ -154,20 +174,21 @@ fn main() -> std::io::Result<()> {
                     dmx_write_throttle =
                         nix::time::clock_gettime(nix::time::ClockId::CLOCK_MONOTONIC)?;
                 }
-                EVENT_TICK_NUM => {
+                Event::Tick => {
                     // Handle timer tick event
                     let _ = ticker.wait();
                     let udp_frames_delta = udp_frames - last_udp_frames;
                     let dmx_frames_delta = dmx_frames - last_dmx_frames;
-                    println!(
-                        "UDP frames: {}, DMX frames: {}",
-                        udp_frames_delta, dmx_frames_delta
-                    );
+                    last_udp_delta = udp_frames_delta;
+                    last_dmx_delta = dmx_frames_delta;
+                    if args.debug {
+                        println!(
+                            "UDP frames: {}, DMX frames: {}",
+                            udp_frames_delta, dmx_frames_delta
+                        );
+                    }
                     last_udp_frames = udp_frames;
                     last_dmx_frames = dmx_frames;
-                }
-                _ => {
-                    println!("Received unknown event: {:?}", event.data());
                 }
             }
         }
